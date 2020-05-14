1b7d9fcfe82b3e773ae4d1596dcfd0a931523784
Bhusting97 <bhusting@spu.edu>
2020-05-13 19:49:31 -0700

    Fixed Extra Char.

--- Domain/House.cs
+++ Domain/House.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Domain
 {
-        public class House
-        {
-            public Guid HouseId { get; set; }
+    public class House
+    {
+        public Guid HouseId { get; set; }
 
-            public string HouseName { get; set; }
+        public string HouseName { get; set; }
 
-            public Guid Channel { get; set; }
-        }
+        public Guid Channel { get; set; }
     }
 }
